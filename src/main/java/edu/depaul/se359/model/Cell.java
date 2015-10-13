704f65fd76714c32c7a4af1789abd97e9f3b3e69
Marlon Garcia <marlongarcia16391@gmail.com>
2015-10-13 12:28:06 -0500

    Adding more functionality to the Cell Class
    I added the comments of the logic of must of this methods.
    
    I added the more functionality to this class

--- src/main/java/edu/depaul/se359/model/Cell.java
+++ src/main/java/edu/depaul/se359/model/Cell.java
@@ -16,6 +16,10 @@ public class Cell {
     private int charge;
     private int dirt;
     private int surface;
+    private Cell rightCell;
+    private Cell leftCell;
+    private Cell upperCell;
+    private Cell lowerCell;
 
     public Cell(int xPosition, int yPosition, int rightPath, int leftPath, int upPath, int downPath, int charge, int dirt, int surface) {
 
@@ -37,11 +41,35 @@ public class Cell {
     public void setPoint(CoordinatePoint point) {
         this.point = point;
     }
-
-    public int getSurface() {
-        return surface;
+    
+    public int getX(){
+    	
+    	return this.point.x;
     }
-
+    
+    public int getY(){
+    	
+    	return this.point.y;
+    	
+    }
+    
+    /*
+     * @returns three types of surfaces
+     * 1. bare floor
+     * 2. low-pile
+     * 3. High-pile carpet
+     */
+    public int getSurface() {
+    	return surface;
+    }
+    
+    /*
+     * sets three types of surfaces
+     * 1. bare floor
+     * 2. low-pile
+     * 3. High-pile carpet
+     * @param surface
+     */
     public void setSurface(int surface) {
         this.surface = surface;
     }
@@ -60,10 +88,27 @@ public class Cell {
                 '}';
     }
 
+    /*
+     * @return the path of the right Cell of this current cell
+     * 1. unknown
+     * 2. it is open
+     * 3. there is an obstacle
+     * 4. there is a stairs
+     */
+    
     public int getRight() {
         return right;
     }
 
+    /*
+     * Sets the path of the right Cell of this current cell
+     * 1. unknown
+     * 2. it is open
+     * 3. there is an obstacle
+     * 4. there is a stairs
+     * @param right
+     */
+    
     public void setRight(int right) {
 
         if (right > 4) {
@@ -79,10 +124,27 @@ public class Cell {
         this.right = right;
     }
 
+    
+    /*
+     * @return the path of the left Cell of this current cell
+     * 1. unknown
+     * 2. it is open
+     * 3. there is an obstacle
+     * 4. there is a stairs
+     */
     public int getLeft() {
         return left;
     }
 
+    
+    /*
+     * Sets the path of the left Cell of this current cell
+     * 1. unknown
+     * 2. it is open
+     * 3. there is an obstacle
+     * 4. there is a stairs
+     * @param left
+     */
     public void setLeft(int left) {
 
         if (left > 4) {
@@ -98,10 +160,26 @@ public class Cell {
         this.left = left;
     }
 
+    
+    /*
+     * @return the path of the above Cell of this current cell
+     * 1. unknown
+     * 2. it is open
+     * 3. there is an obstacle
+     * 4. there is a stairs
+     */
     public int getUp() {
         return up;
     }
 
+    /*
+     * Sets the path of the above Cell of this current cell
+     * 1. unknown
+     * 2. it is open
+     * 3. there is an obstacle
+     * 4. there is a stairs
+     * @param up
+     */
     public void setUp(int up) {
 
         if (up > 4) {
@@ -117,10 +195,26 @@ public class Cell {
         this.up = up;
     }
 
+    
+    /*
+    * @return the path of the below Cell of this current cell
+    * 1. unknown
+    * 2. it is open
+    * 3. there is an obstacle
+    * 4. there is a stairs
+    */
     public int getDown() {
         return down;
     }
 
+    /*
+     * Sets the path of the below Cell of this current cell
+     * 1. unknown
+     * 2. it is open
+     * 3. there is an obstacle
+     * 4. there is a stairs
+     * @param down
+     */
     public void setDown(int down) {
 
 
@@ -137,6 +231,8 @@ public class Cell {
         this.down = down;
     }
 
+   
+    
     public boolean getCharge() {
         return charge == 1;
     }
@@ -164,4 +260,83 @@ public class Cell {
 
         this.dirt = dirt;
     }
+    
+    /*
+     * @return the Cell that is to the right of this Cell. 
+     * there is a posibility that this direction could be null because is blocked
+     */
+    public Cell getRightCell(){
+    	
+    	return this.rightCell;
+    }
+    
+    /*
+     * sets the Cell that is to the right of this Cell.
+     * @param RCell. there is a posibility that could be null if the path for this direction is blocked
+     */
+    
+    public void setRightCell(Cell RCell){
+    	
+    	this.rightCell = RCell;
+    }
+    
+    
+    /*
+     * @return the Cell that is to the Left of this Cell. 
+     * there is a posibility that this direction could be null because is blocked
+     */
+    public Cell getLeftCell(){
+    	
+    	return this.leftCell;
+    }
+    
+    /*
+     * sets the Cell that is to the left of this Cell.
+     * @param LCell. there is a posibility that could be null if the path for this direction is blocked
+     */
+    
+    public void setLeftCell(Cell LCell){
+    	
+    	this.leftCell = LCell;
+    }
+    
+    /*
+     * @return the Cell that is to the Upper of this Cell. 
+     * there is a posibility that this direction could be null because is blocked
+     */
+    public Cell getUpperCell(){
+    	
+    	return this.upperCell;
+    }
+    
+    /*
+     * sets the Cell that is to the Upper of this Cell.
+     * @param UCell. there is a posibility that could be null if the path for this direction is blocked
+     */
+    
+    public void setUpperCell(Cell UCell){
+    	
+    	this.upperCell = UCell;
+    }
+    
+    /*
+     * @return the Cell that is to the Lower of this Cell. 
+     * there is a posibility that this direction could be null because is blocked
+     */
+    public Cell getLowerCell(){
+    	
+    	return this.lowerCell;
+    }
+    
+    /*
+     * sets the Cell that is to the Lower of this Cell.
+     * @param LCell. there is a posibility that could be null if the path for this direction is blocked
+     */
+    
+    public void setLowerCell(Cell LCell){
+    	
+    	this.lowerCell = LCell;
+    }
+    
+        
 }
