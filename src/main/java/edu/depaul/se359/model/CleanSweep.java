fca6e7ebcd481bbd92e57cb7bb911f8d73e7e4eb
Marlon Garcia <marlongarcia16391@gmail.com>
2015-10-22 16:45:10 -0500

    partial done mapping of the house cell layout
    this method is partial done It will be finish by me later

--- src/main/java/edu/depaul/se359/model/CleanSweep.java
+++ src/main/java/edu/depaul/se359/model/CleanSweep.java
@@ -26,6 +26,30 @@ public class CleanSweep {
 		
 	}
 	
+	public Map<Integer, Cell> addingCellsToHouseMap(Map<Integer, Cell> houseMap){
+		
+		Integer CellCount = this.HouseMap.size();
+		
+		if(this.HouseMap.containsValue(this.CurrentCell)){
+			
+			// it contains a value of the current cell don't added 
+			
+		}
+		
+		else {
+			// note A map maps a key to a value. If you have a value and you know the map contains this value, why do you need the key anymore
+			// if it doesn't contain add it 
+			
+			this.HouseMap.put(CellCount, this.CurrentCell);
+				
+		}
+		
+		//to continue later
+	
+		return this.HouseMap;
+		
+	}
+	
 	
 	
 	
