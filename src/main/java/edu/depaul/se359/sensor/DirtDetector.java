8e15e85b6af0acc8458768bf60e2e82b1c105f6a
Eric Pfeiffer <computerfreak@computerfr33k.com>
2015-10-23 14:56:56 -0500

    Added Dirt Detector Sensor.

---
+++ src/main/java/edu/depaul/se359/sensor/DirtDetector.java
@@ -0,0 +1,19 @@
+package edu.depaul.se359.sensor;
+
+import edu.depaul.se359.model.Cell;
+
+/**
+ * Created by eric on 10/23/15.
+ */
+public class DirtDetector {
+
+    /**
+     * Determines if the given cell is dirty
+     *
+     * @param cell
+     * @return boolean
+     */
+    public boolean isDirty(Cell cell) {
+        return cell.getDirt() > 0;
+    }
+}
