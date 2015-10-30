59d8a76b0103f0f8b990c7584ef14455e51fa31a
Eric Pfeiffer <computerfreak@computerfr33k.com>
2015-10-30 11:12:46 -0500

    create Report folder if it doesn't exist when starting up the logger.

--- src/main/java/edu/depaul/se359/model/LogFile.java
+++ src/main/java/edu/depaul/se359/model/LogFile.java
@@ -1,5 +1,6 @@
 package edu.depaul.se359.model;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -10,6 +11,11 @@ public class LogFile {
     private static LogFile ourInstance = new LogFile();
 
     private LogFile() {
+        // create folder if it doesn't exist
+        File folder = new File("./Report");
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
     }
 
     public static LogFile getInstance() {
@@ -20,6 +26,7 @@ public class LogFile {
         Logger.getLogger("LogFile").log(logLevel, report);
 
         // append to file
+
         try {
             FileWriter fw = new FileWriter("Report/report.log", true);
             fw.write("[" + logLevel + "] " + report + "\n");
