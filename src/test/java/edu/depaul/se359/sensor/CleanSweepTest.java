e798b20dd9a7d479fcffef70ad26cbc755f06e4e
Eric Pfeiffer <computerfreak@computerfr33k.com>
2015-11-12 17:16:22 -0600

    Update clean sweep test to get the charge station from the layout file.

--- src/test/java/edu/depaul/se359/sensor/CleanSweepTest.java
+++ src/test/java/edu/depaul/se359/sensor/CleanSweepTest.java
@@ -1,29 +1,34 @@
 package edu.depaul.se359.sensor;
 
-import edu.depaul.se359.model.Cell;
-import edu.depaul.se359.model.CleanSweep;
-import edu.depaul.se359.model.HomeLayoutPlanMap;
+import edu.depaul.se359.model.*;
 import edu.depaul.se359.service.LayoutParser;
 import junit.framework.TestCase;
 import org.junit.Test;
 
 public class CleanSweepTest extends TestCase {
 
-    private HomeLayoutPlanMap layouts;
-
     @Test
     public void testCleanSweep() throws Exception {
 
-        layouts = LayoutParser.parseHomeLayout(getClass().getResourceAsStream("/FloorPlans/FloorPlanLayoutHome2.json"));
+        HomeLayoutPlanMap layouts = LayoutParser.parseHomeLayout(getClass().getResourceAsStream("/FloorPlans/FloorPlanLayoutHome2.json"));
 
-        int xPosition = 0, yPosition = 0, rightPath = 4, leftPath = 4, upPath = 4, downPath = 4;
+        Cell chargeStation = null;
+        for (Floor floor : layouts.getHomeLayout().getFloors()) {
+            for (Room room : floor.getRooms()) {
+                for (Cell cell : room.getCells()) {
+                    if (cell.CheckisaChargingStation()) {
+                        chargeStation = cell;
+                    }
+                }
+            }
+        }
 
-        Cell chargeStation = new Cell(xPosition, yPosition, rightPath, leftPath, upPath, downPath, false, 2, 1);
+        // Make sure there is at least one charge station / Home Cell
+        assertNotNull(chargeStation);
 
         CleanSweep cleanSweep = new CleanSweep(chargeStation, layouts.getHomeLayout());
 
         assertTrue(0 == cleanSweep.getCurrentPosition().getX());
         assertTrue(0 == cleanSweep.getCurrentPosition().getY());
-
     }
 }
